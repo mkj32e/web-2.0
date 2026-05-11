494fbf797073830d21004db53762600959bd493d
maxime <mkj32e@gmail.com>
2026-05-11 18:45:29 +0200

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, send_from_directory
+import json, os
+
+app = Flask(__name__)
+SCORE_FILE = 'score.json'
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory('.', filename)
+
+@app.route('/score', methods=['POST'])
+def save_score():
+    data = request.json
+    if os.path.exists(SCORE_FILE):
+        with open(SCORE_FILE, 'r') as f:
+            score = json.load(f)
+    else:
+        score = []
+    score.append({'pseudo': data['pseudo'], 'kills': data['kills']})
+    score.sort(key=lambda x: x['kills'], reverse=True)
+    score = score[:10]
+    with open(SCORES_FILE, 'w') as f:
+        json.dump(score, f)
+    return jsonify({'ok': True})
+
+@app.route('/score', methods=['GET'])
+def get_score():
+    if os.path.exists(SCORE_FILE):
+        with open(SCORE_FILE, 'r') as f:
+            return jsonify(json.load(f))
+    return jsonify([])
+
+if __name__ == '__main__':
+    app.run(debug=True)
